ed08d56551844a0ff65df19fdd86f524b3b6f0f7
Stefan Griffin <griffinstef39@gmail.com>
2020-11-03 18:00:40 -0500

    Added greet.html

--- src/data/views.py
+++ src/data/views.py
@@ -1,7 +1,15 @@
-from flask import render_template
+from flask import render_template, request
 from src import app
 
 
 @app.route("/data/")
 def data():
     return render_template('data.html')
+
+@app.route("/greet/")
+def greet():
+    return render_template('greet.html', name=request.args.get('name'))
+
+
+
+
