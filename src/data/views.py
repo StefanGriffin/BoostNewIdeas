6e60cba8b4c1f3d516f52a878a50239fc7a7ace4
Stefan Griffin <griffinstef39@gmail.com>
2020-11-03 18:38:16 -0500

    GET and POST

--- src/data/views.py
+++ src/data/views.py
@@ -4,11 +4,14 @@ from src import app
 
 @app.route("/data/")
 def data():
-    return render_template('data.html')
+    return render_template("data.html")
 
-@app.route("/greet/")
+@app.route("/greet/", methods=["GET", "POST"])
 def greet():
-    return render_template('greet.html', name=request.args.get('name'))
+    if request.method == "GET":
+        return render_template("data_html")
+    if request.method == "POST":
+        return render_template("greet.html", name=request.form.get("name"))
 
 
 
